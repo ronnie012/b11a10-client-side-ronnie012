f9b2b29560777fbba4e1cf4caab179203ec62819
ronnie012 <ronnie.247365.1@gmail.com>
2025-05-22 00:19:28 +0600

    Added home page user interface and initial content.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold mb-4">Welcome to GigConnect!</h2>
+            <p>This is the Home Page content.</p>
+        </div>
+    );
+};
+
+export default HomePage;
